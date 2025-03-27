797c18e172ccb0451eb1d80ff51d09234faa9557
randy <tom@randy.gg>
2025-03-27 15:15:10 +0700

    redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,36 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   redirects: {
-    "/blog": "/",
+
+    "/blog": "https://randyprime.beehiiv.com/",
+    "/writings": "https://randyprime.beehiiv.com/",
+
+    "/arcane": "https://s.team/a/2571560",
+    "/changelog": "https://alpinegames.substack.com/",
+    "/font": "https://randyprime.itch.io/randygg-monospace-font",
+
+    "/kofi": "https://ko-fi.com/bigrando420",
+    "/donate": "https://ko-fi.com/bigrando420",
+    "/ko-fi": "https://ko-fi.com/bigrando420",
+    "/fund": "https://ko-fi.com/bigrando420",
+
+    "/learn": "https://private-community.carrd.co/",
+    "/list": "https://randyprime.beehiiv.com",
+
+    "/patreon": "https://www.patreon.com/alpinegames",
+    "/patty": "https://www.patreon.com/alpinegames",
+
+    "/playlists": "https://open.spotify.com/user/c5i6oure30sx891a24pnr19xn/playlists",
+    
+
+    "/terrafactor": "https://terrafactor.randy.gg",
+    
+    "/team": "mailto:contact@randy.gg",
+    "/work": "mailto:contact@randy.gg",
+    "/contact": "mailto:contact@randy.gg",
+
+    "/res": "404", // TODO, .md blog post
+
   },
   site: "https://randy.gg",
   integrations: [tailwind(), sitemap(), icon()],
