ef56c8f107825a0d1e78f14ccc6a199847b9a1ed
randy <tom@randy.gg>
2026-01-19 19:00:40 +0700

    presskit

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   redirects: {
+    
+    "/terrafactor-presskit": "https://drive.google.com/drive/folders/1nRIOWVQbj51GuuETTFcufOqizi7A1gvI?usp=sharing",
 
     "/community": "https://www.skool.com/dev-fellowship-8002",
 
